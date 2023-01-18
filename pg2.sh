83b78b563c0eaaa87c9657792e33b33a21e26eae
b666k777@gmail.com <thasnisaleem1999@gmail.com>
2023-01-18 15:53:31 +0530

    third commit

---
+++ pg2.sh
@@ -0,0 +1,11 @@
+echo "Enter  two Nmber"
+read a
+read b
+s=`expr $a + $b`
+echo "sum = $s"
+d=`expr $a - $b`
+echo "Difference = $d"
+p=`expr $a \* $b`
+echo "Product = $p"
+D=`expr $a / $b`
+echo "Division = $D"
